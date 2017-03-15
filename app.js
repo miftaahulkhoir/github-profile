89f1ff4a93de306edda579fb5a0db1048b429f8a
Miftahul Khoir <miftaahulkhoir@gmail.com>
2017-03-16 00:34:13 +0700

    finish interface in terminal

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let profile = require('./profile.js')
 
-profile.get('miftaahulkhoir')
-profile.get('miftaahulkhoir2')
-profile.get('rizafahmi')
-profile.get('Yofri')
+let profiles = process.argv.slice(2);
+
+profiles.map(profile.get)
